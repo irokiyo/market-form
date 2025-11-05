c2db5cc54e5a552353ca86ae599ca4412b6fda10
nico <yuuuuuuuu.13@gmail.com>
2025-11-06 01:04:00 +0900

    ルーティング設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [ItemController::class, 'index'])->name('index'); //商品一覧画面（トップ画面）
+Route::get('/register', [ItemController::class, 'register'])->name('register'); //会員登録画面
+Route::get('/login', [ItemController::class, 'login'])->name('login'); //ログイン画面
+Route::get('/item/{item_id}', [ItemController::class, 'show'])->name('show'); //商品詳細画面
+Route::get('/purchase/{item_id}', [ItemController::class, 'purchase'])->name('purchase'); //商品購入画面
+Route::get('/purchase/address/{item_id}', [ItemController::class, 'address'])->name('purchase.address'); //住所変更ページ
+Route::get('/sell', [ItemController::class, 'sell'])->name('sell'); //商品出品画面
+Route::get('/mypage', [ItemController::class, 'mypage'])->name('mypage'); //プロフィール画面
+Route::get('/mypage/profile', [ItemController::class, 'edit'])->name('mypage.edit'); //プロフィール編集画面
