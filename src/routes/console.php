551416a1c948c2d21b188696a7ea75acd1cbffe0
nico <yuuuuuuuu.13@gmail.com>
2025-12-17 11:51:09 +0900

    fix CI working directory

--- src/routes/console.php
+++ src/routes/console.php
@@ -2,18 +2,8 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Command;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+Artisan::command('inspire', function (Command $command) {
+    $command->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
